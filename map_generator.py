4c323dc6f0d45de6b47c5bf12f475344e9a9e729
dreamking60 <dreamlyboyczh@foxmail.com>
2025-03-08 16:02:55 -0600

    Middle work for background

--- map_generator.py
+++ map_generator.py
@@ -6,18 +6,35 @@ import pygame
 TRACK_COUNT = 5
 SIDE_WIDTH = 220
 
-def draw_map(screen):
-    #Draw 5 tracks on the screen
-    screen.fill("gray")
-    track_width = (screen.get_width()- 2 * SIDE_WIDTH) // (TRACK_COUNT)
+
+class MapGenerator:
+    def __init__(self, screen):
+        #Draw 5 tracks on the screen
+        self.track_width = (screen.get_width()- 2 * SIDE_WIDTH) // (TRACK_COUNT)
+
+        # Deal with the track image    
+        track_bg = pygame.image.load("./resources/image/track.jpeg")
+        TRACK_X = track_bg.get_width() // 2 - 25
+        TRACK_Y = 0
+        TRACK_WIDTH = 50
+        TRACK_HEIGHT = track_bg.get_height()
+        self.track_image = track_bg.subsurface(pygame.Rect(TRACK_X, TRACK_Y, TRACK_WIDTH, TRACK_HEIGHT))
+
+
     
-    # Draw the tarck lines    
-    for i in range(1, TRACK_COUNT):
-        x_pos = SIDE_WIDTH + i * track_width
-        pygame.draw.line(screen, "black", (x_pos, 0), (x_pos, screen.get_height()), 3)
+    def draw_map(self, screen):
+        screen.fill("gray")        
         
-    # Draw left side with white
-    pygame.draw.rect(screen, "white", (0, 0, SIDE_WIDTH, screen.get_height()))
-    
-    # Draw right side with white
-    pygame.draw.rect(screen, "white", (screen.get_width() - SIDE_WIDTH, 0, SIDE_WIDTH, screen.get_height()))
+        # Draw the tarck lines    
+        for i in range(1, TRACK_COUNT):
+            x_pos = SIDE_WIDTH + i * self.track_width
+            pygame.draw.line(screen, "black", (x_pos, 0), (x_pos, screen.get_height()), 3)
+            screen.blit(self.track_image, (x_pos, 0))
+                    
+        # Draw left side with white
+        pygame.draw.rect(screen, "white", (0, 0, SIDE_WIDTH, screen.get_height()))
+        
+        # Draw right side with white
+        pygame.draw.rect(screen, "white", (screen.get_width() - SIDE_WIDTH, 0, SIDE_WIDTH, screen.get_height()))
+
+
